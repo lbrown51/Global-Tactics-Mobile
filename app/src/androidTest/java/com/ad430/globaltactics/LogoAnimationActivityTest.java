09b8541fb1154fbddc81d830cbc65a3f8411caac
Farhad <fbahrehmand82@gmail.com>
2020-10-10 23:01:30 -0700

    logo animation

---
+++ app/src/androidTest/java/com/ad430/globaltactics/LogoAnimationActivityTest.java
@@ -0,0 +1,37 @@
+package com.ad430.globaltactics;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogoAnimationActivityTest {
+    @Rule
+    public ActivityTestRule<LogoAnimationActivity> activityTestRule =
+            new ActivityTestRule<>(LogoAnimationActivity.class);
+
+    private LogoAnimationActivity lAActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        lAActivity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void hasLogoLetters() {
+        View view = lAActivity.findViewById(R.id.lALogo);
+
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        lAActivity = null;
+    }
+}
