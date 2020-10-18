c667096d2dd8cdf7082060127ba451dd7a3d5f99
Farhad <fbahrehmand82@gmail.com>
2020-10-18 13:03:10 -0700

    seperate fragmant test and add scroll view to the fragment

---
+++ app/src/androidTest/java/com/ad430/globaltactics/AboutUsFragmentTest.java
@@ -0,0 +1,69 @@
+package com.ad430.globaltactics;
+
+import android.content.Intent;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.intent.matcher.UriMatchers.hasHost;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+public class AboutUsFragmentTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void aboutUsFragmentsAllTextViews() {
+        onView(withId(R.id.tvAboutUs)).check(matches(withText(R.string.about_us)));
+        onView(withId(R.id.tvParagraphOne)).check(matches(withText(R.string.paragraph_one)));
+        onView(withId(R.id.tvParagraphTwo)).check(matches(withText(R.string.paragraph_two)));
+        onView(withId(R.id.tvParagraphThree)).check(matches(withText(R.string.paragraph_three)));
+        onView(withId(R.id.tvParagraphFour)).check(matches(withText(R.string.paragraph_four)));
+        onView(withId(R.id.tvBorderLine)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void homeScreenFragmentHasSocialLinks() {
+        onView(withId(R.id.ivLinkedin)).check(matches(isDisplayed()));
+        onView(withId(R.id.ivFacebook)).check(matches(isDisplayed()));
+        onView(withId(R.id.ivTwitter)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnLinkedinIcon() {
+        onView(withId(R.id.ivLinkedin)).perform(scrollTo(), click());
+
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("www.linkedin.com")))));
+    }
+
+    @Test
+    public void clickOnFacebookIcon() {
+        onView(withId(R.id.ivFacebook)).perform(scrollTo(), click());
+
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("www.facebook.com")))));
+    }
+
+    @Test
+    public void clickOnTwitterIcon() {
+        onView(withId(R.id.ivTwitter)).perform(scrollTo(), click());
+
+        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("twitter.com")))));
+    }
+}
--- app/src/androidTest/java/com/ad430/globaltactics/MainActivityTest.java
+++ app/src/androidTest/java/com/ad430/globaltactics/MainActivityTest.java
@@ -14,17 +14,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static androidx.test.espresso.intent.matcher.UriMatchers.hasHost;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static org.hamcrest.Matchers.allOf;
 
 import static org.junit.Assert.*;
 
@@ -34,9 +26,6 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
-
-    @Rule
-    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
     public void hasHelloWorld() {
@@ -55,43 +44,5 @@ public class MainActivityTest {
         onView(withId(R.id.privacy_placeholder_text))
                 .check(matches(withText(R.string.privacy_placeholder)));
 
-    }
-
-    @Test
-    public void aboutUsFragmentsAllTextViews() {
-        onView(withId(R.id.tvAboutUs)).check(matches(withText(R.string.about_us)));
-        onView(withId(R.id.tvParagraphOne)).check(matches(withText(R.string.paragraph_one)));
-        onView(withId(R.id.tvParagraphTwo)).check(matches(withText(R.string.paragraph_two)));
-        onView(withId(R.id.tvParagraphThree)).check(matches(withText(R.string.paragraph_three)));
-        onView(withId(R.id.tvParagraphFour)).check(matches(withText(R.string.paragraph_four)));
-        onView(withId(R.id.tvBorderLine)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void homeScreenFragmentHasSocialLinks() {
-        onView(withId(R.id.ivLinkedin)).check(matches(isDisplayed()));
-        onView(withId(R.id.ivFacebook)).check(matches(isDisplayed()));
-        onView(withId(R.id.ivTwitter)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void clickOnLinkedinIcon() {
-        onView(withId(R.id.ivLinkedin)).perform(click());
-
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("www.linkedin.com")))));
-    }
-
-    @Test
-    public void clickOnFacebookIcon() {
-        onView(withId(R.id.ivFacebook)).perform(click());
-
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("www.facebook.com")))));
-    }
-
-    @Test
-    public void clickOnTwitterIcon() {
-        onView(withId(R.id.ivTwitter)).perform(click());
-
-        intended(allOf(hasAction(Intent.ACTION_VIEW), hasData(allOf(hasHost("twitter.com")))));
     }
 }
