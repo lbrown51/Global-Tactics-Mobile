05eaa986158490c201356ebcd8e27aa44e3cb522
Lenny Brown <penn2014@gmail.com>
2020-10-19 18:50:44 -0700

    Tests now pass for navigation
    Also added a homescreen fragment test file.

--- app/src/androidTest/java/com/ad430/globaltactics/MainActivityTest.java
+++ app/src/androidTest/java/com/ad430/globaltactics/MainActivityTest.java
@@ -17,8 +17,11 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.junit.Assert.*;
@@ -32,41 +35,68 @@ public class MainActivityTest {
 
     @Test
     public void navigationDrawerHasCorrectItems() {
-        onView(withText("Home"))
-                .check(matches(isDisplayed()));
-
         onView(withId(R.id.drawer_layout))
                 .check(matches(isClosed(Gravity.LEFT)))
                 .perform(DrawerActions.open());
 
         onView(withId(R.id.navigation_view))
+                .check(matches(hasDescendant(withText(R.string.home_fragment_title))));
+        onView(withId(R.id.navigation_view))
+                .check(matches(hasDescendant(withText(R.string.about_us_fragment_title))));
+        onView(withId(R.id.navigation_view))
+                .check(matches(hasDescendant(withText(R.string.blog_fragment_title))));
+        onView(withId(R.id.navigation_view))
+                .check(matches(hasDescendant(withText(R.string.events_fragment_title))));
+        onView(withId(R.id.navigation_view))
+                .check(matches(hasDescendant(withText(R.string.privacy_policy_fragment_title))));
+    }
+
+    @Test
+    public void canNavigateToAllFragments() throws InterruptedException {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.navigation_view))
                 .perform(NavigationViewActions.navigateTo(R.id.aboutUsFragment));
+        onView(withId(R.id.topAppBar))
+                .check(matches(hasDescendant(withText(R.string.about_us_fragment_title))));
+
+        Thread.sleep(500);
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.open())
+                .check(matches(isOpen()));
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.blogFragment));
+        onView(withId(R.id.topAppBar))
+                .check(matches(hasDescendant(withText(R.string.blog_fragment_title))));
+
+        Thread.sleep(500);
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.open())
+                .check(matches(isOpen()));
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.eventsFragment));
+        onView(withId(R.id.topAppBar))
+                .check(matches(hasDescendant(withText(R.string.events_fragment_title))));
+
+        Thread.sleep(500);
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.open())
+                .check(matches(isOpen()));
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.privacyPolicyFragment));
+        onView(withId(R.id.topAppBar))
+                .check(matches(hasDescendant(withText(R.string.privacy_policy_fragment_title))));
+
+        Thread.sleep(500);
+        onView(withId(R.id.drawer_layout))
+                .perform(DrawerActions.open())
+                .check(matches(isOpen()));
+        onView(withId(R.id.navigation_view))
+                .perform(NavigationViewActions.navigateTo(R.id.homeScreenFragment));
+        onView(withId(R.id.topAppBar))
+                .check(matches(hasDescendant(withText(R.string.home_fragment_title))));
     }
-  
-//    @Test
-//    public void allFragmentsDisplayed() {
-//        onView(withId(R.id.about_placeholder_text))
-//                .check(matches(withText(R.string.about_us_placeholder)));
-//        onView(withId(R.id.blog_placeholder_text))
-//                .check(matches(withText(R.string.blog_placeholder)));
-//        onView(withId(R.id.events_placeholder_text))
-//                .check(matches(withText(R.string.events_placeholder)));
-//        onView(withId(R.id.privacy_placeholder_text))
-//                .check(matches(withText(R.string.privacy_placeholder)));
-//
-//    }
-//
-    @Test public void homeScreenFragmentHasMotto() {
-        onView(withId(R.id.motto_placeholder)).check(matches(withText(R.string.motto_text)));
-    }
-//
-//    @Test public void homeScreenFragmentHasGTLogo() {
-//        onView(withId(R.id.gtLogo)).check(matches(isDisplayed()));
-//    }
-//
-//    @Test public void homeScreenFragmentHasGTLogoInAppBar() {
-//        onView((withId(R.id.logo))).check(matches(isDisplayed()));
-//    }
 
 
 }
