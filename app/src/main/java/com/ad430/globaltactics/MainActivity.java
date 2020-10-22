d73f47f0a8cd5c5b3b0aa24a1ded13594bfcf4d2
Lenny Brown <penn2014@gmail.com>
2020-10-21 19:57:04 -0700

    Firebase functions and trigger on firestore now work

--- app/src/main/java/com/ad430/globaltactics/MainActivity.java
+++ app/src/main/java/com/ad430/globaltactics/MainActivity.java
@@ -1,32 +1,58 @@
 package com.ad430.globaltactics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.customview.widget.Openable;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
-
-import org.jetbrains.annotations.NotNull;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "MAIN ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> contactFormInfo = new HashMap<>();
+        contactFormInfo.put("firstName", "Lenny");
+        contactFormInfo.put("lastName", "Brown");
+        contactFormInfo.put("topic", "Consultancy");
+        contactFormInfo.put("email", "penn2014@gmail.com");
+
+        db.collection("requests")
+                .add(contactFormInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
         setupNavigation();
     }
