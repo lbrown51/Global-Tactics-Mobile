88b13b9b2ca37e0326cfcd83df3ee6086ee2bdaf
Samuel Richardson <toolbax@gmail.com>
2020-11-05 21:11:48 -0800

    Add contact page testing

--- app/src/androidTest/java/com/ad430/globaltactics/ContactFragmentTest.java
+++ app/src/androidTest/java/com/ad430/globaltactics/ContactFragmentTest.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.testing.FragmentScenario;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
@@ -17,12 +18,18 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertNotNull;
 
+
 public class ContactFragmentTest {
 
     @Test
     public void aboutUsFragmentsAllTextViews() {
         FragmentScenario<ContactFragment> fragmentScenario = FragmentScenario.launchInContainer(ContactFragment.class);
 
+        onView(withId(R.id.first_name)).perform(typeText("test"));
+        onView(withId(R.id.last_name)).perform(typeText("test last"));
+        onView(withId(R.id.your_email)).perform(typeText("testing@gmail.com"));
+        onView(withId(R.id.your_subject)).perform(typeText("test subject"));
+        onView(withId(R.id.your_message)).perform(typeText("test message"));
 
         onView(withId(R.id.post_message)).check(matches(withText(R.string.contact_form_button)));
         //   onView(withId(R.id.tvBorderLine)).check(matches(isDisplayed()));
