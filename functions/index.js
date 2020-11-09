9a3115a9027dff50651e99db47fcaad14e60c042
Lenny Brown <penn2014@gmail.com>
2020-11-09 14:32:27 -0800

    All of the things are here.

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,11 @@ const functions = require("firebase-functions");
 
 const admin = require("firebase-admin");
 admin.initializeApp();
+const db = admin.firestore();
 
 const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
   service: "gmail",
-  auth: {
-  },
 });
 
 exports.sendContactInformation = functions.firestore
@@ -24,7 +23,7 @@ exports.sendContactInformation = functions.firestore
       from: "gtcontactform@gmail.com",
       to: "lenny.casey.brown@gmail.com",
       subject: "GT Contact Form",
-      text: '' + from + name + topic + message,
+      text: '' + String(email) + name + topic + message,
     };
 
     return transporter.sendMail(mailOptions, (err, info) => {
@@ -35,3 +34,69 @@ exports.sendContactInformation = functions.firestore
       return { info: "SENT REQUEST FORM" };
     });
   });
+
+const cheerio = require('cheerio');
+const got = require('got');
+
+exports.fetchAndParseOurExpertsHTML = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const ourExpertsBatchAdd = db.batch();
+  const ourExperts = {};
+  const ourExpertsURL = "https://www.globaltactics.co/who-we-are/our-experts.html";
+
+  got(ourExpertsURL).then(response => {
+    const $ = cheerio.load(response.body);
+    const ourExpertSections = $("section");
+    
+    let expertCount = 0;
+    ourExpertSections.each((i, section) => {
+      const ourExpertInnerAreas = $(section).find("div.area_inner");
+
+      ourExpertInnerAreas.each((i, innerArea) => {
+        const ourExpertName = $(innerArea).find("h2").text();
+        const ourExpertTitle = $(innerArea).find("h4").text();
+
+        if(ourExpertName !== "" && ourExpertTitle !== "") {
+          const ourExpertSpecialties = $(innerArea).find("small").text();          
+          let ourExpertLinkedIn = $(innerArea).find("a").attr("href");
+          if (ourExpertLinkedIn === undefined) {
+            ourExpertLinkedIn = "";
+          }
+
+          $(innerArea).find("small").remove();
+          $(innerArea).find("a").remove();
+          const ourExpertDescription = $(innerArea).find("p").text();
+
+
+          const ourExpert = {
+            id: expertCount++,
+            name: ourExpertName,
+            title: ourExpertTitle,
+            specialties: ourExpertSpecialties,
+            description: ourExpertDescription,
+            linkedin: ourExpertLinkedIn
+          };
+
+          if (!(ourExpert.name in ourExperts)) {
+            ourExperts[ourExpert.name] = ourExpert;
+            const ourExpertsNewRef = db.collection("experts").doc(ourExpert.id.toString());
+            ourExpertsBatchAdd.set(ourExpertsNewRef, ourExpert);
+          }
+        }
+      });
+    });
+
+    ourExpertsBatchAdd.commit()
+      .then(() => {
+        res.json({result: `it worked!`});
+        return null;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return null;
+  }).catch(err => {
+    console.log(err);
+  });
+});
