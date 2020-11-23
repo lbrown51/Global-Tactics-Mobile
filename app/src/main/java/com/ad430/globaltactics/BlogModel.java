6598839c4c73d6ca42efe03a3430a46c56e514aa
ahmadafrasiyab <afrasiyab.ahmad@hotmail.com>
2020-11-22 19:24:10 -0800

    added label filtering functionality

--- app/src/main/java/com/ad430/globaltactics/BlogModel.java
+++ app/src/main/java/com/ad430/globaltactics/BlogModel.java
@@ -9,16 +9,19 @@ import com.google.api.services.blogger.Blogger;
 import com.google.api.services.blogger.model.PostList;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class BlogModel {
     private final String TAG = BlogModel.class.getSimpleName();
     private final MutableLiveData<PostList> postList;
     private Blogger blogger;
     private TaskRunner taskRunner;
-
+    private ArrayList <String> labels;
+    private String [] listOfLabels = {"Entrepreneurship","Leadership", "Risks", "Africa", "China", "Europe", "India", "Latin America", "MENA"};
     BlogModel() {
         postList = new MutableLiveData<>();
         taskRunner = new TaskRunner();
+
 
         String BLOG_URL = "https://www.gtperspectives.com/";
         String BLOG_ID = "5044990419048085041";
@@ -29,10 +32,15 @@ public class BlogModel {
         blogger = new Blogger(HTTP_TRANSPORT, JSON_FACTORY, null);
 
         Blogger.Posts.List postListGetAllAction = null;
+
         try {
             postListGetAllAction = blogger.posts().list(BLOG_ID);
+            //add labels to postListGetAllAction
+            addLabelsToPostListGetAllAction(postListGetAllAction);
             postListGetAllAction.setKey("AIzaSyC_B6br4L6-nP2F-R-4mLycGmfxddyX3Bg");
             postListGetAllAction.setMaxResults(5L);
+
+
 
             taskRunner.executeAsync(new BlogAsyncTask(postListGetAllAction), postList::setValue);
         } catch (IOException e) {
@@ -42,5 +50,9 @@ public class BlogModel {
 
     MutableLiveData<PostList> getPostList() {
         return postList;
+    }
+    private void addLabelsToPostListGetAllAction(Blogger.Posts.List postListGetAllAction) {
+        for (String label:listOfLabels
+             )  postListGetAllAction.setLabels(label);
     }
 }
