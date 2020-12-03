34b3e9372b89264cb21b65758bf0007264d0a2f3
Farhad <fbahrehmand82@gmail.com>
2020-12-03 15:49:42 -0800

    non profit text view size

--- app/src/main/java/com/ad430/globaltactics/adapters/OurSuccessTabListAdapter.java
+++ app/src/main/java/com/ad430/globaltactics/adapters/OurSuccessTabListAdapter.java
@@ -1,6 +1,9 @@
 package com.ad430.globaltactics.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +15,20 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ad430.globaltactics.R;
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import io.opencensus.internal.Utils;
 
 public class OurSuccessTabListAdapter extends RecyclerView.Adapter<OurSuccessTabListAdapter.ViewHolder> {
 
@@ -28,7 +41,7 @@ public class OurSuccessTabListAdapter extends RecyclerView.Adapter<OurSuccessTab
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivOurSuccessFlag, ivOurSuccessBackground;
+        ImageView ivOurSuccessFlag;
         TextView tvOurSuccessTitle, tvOurSuccessDescription;
 
         public ViewHolder(@NonNull final View itemView) {
@@ -51,11 +64,13 @@ public class OurSuccessTabListAdapter extends RecyclerView.Adapter<OurSuccessTab
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         HashMap<String,String> dataMap = list.get(position);
-        holder.itemView.setTag(position);
+//        holder.itemView.setTag(position);
 
         String imageUrl = dataMap.get("imageUrl");
 
-        Picasso.get().load(imageUrl).into(holder.ivOurSuccessFlag);
+        Glide.with(this.context).load(imageUrl).into(holder.ivOurSuccessFlag);
+//        Picasso.get().load(imageUrl).into(holder.ivOurSuccessFlag);
+
         holder.tvOurSuccessTitle.setText(dataMap.get("title").trim());
         holder.tvOurSuccessDescription.setText(dataMap.get("description").trim());
     }
